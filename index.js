eb74c15072640496e38230b94e7582ec899d647d
flh6 <talk2florencehanson@gmail.com>
2023-06-02 17:10:15 -0700

    input various codes for the landing page

---
+++ index.js
@@ -0,0 +1,15 @@
+// Created by Florence Hanson
+
+alert ("WELCOME ❤️❤️")
+function fun() {
+var element = document.getElementById("toggle1");
+element.classList.toggle("fa-times");
+var element1 = document.getElementById("toggle2");
+element1.classList.toggle("div2");
+
+var x = document.getElementById("extra")
+if(x.style.display === "block")
+x.style.display = "none";
+else
+x.style.display = "block";
+}
